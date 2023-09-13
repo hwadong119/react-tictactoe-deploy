4b9392b38cd70dd500b458f4e1a28bbfcb8f4b7c
hwadong119 <jeonghwayun0119@naver.com>
2023-09-13 17:40:46 +0900

    initial commit

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import './Square.css'
+
+const Square = ({ onClick, value }) => {
+
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  )
+
+}
+
+export default Square;
